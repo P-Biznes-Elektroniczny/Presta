401c07d2ea5581971432d9ffd7f684bbb4b53c75
root <root@efilmy.best>
2020-11-15 05:29:43 -0500

    initial commit

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'user',
+    'database_password' => '3jsi@jk$',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'w09qFf7XdOr1AX9K1X3XRyzasGeK7EFwrLT9rIxYUTXyu68UziLwcU0u',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2020-10-17',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'PmRFs66DVvQUAMFAsdIbFgwEN2ywM5zd5y0dDbd9wAciCahUR6Y4xObO',
+    'cookie_iv' => 'PcRceYg6',
+    'new_cookie_key' => 'def00000245fe5d7887bcbf8112674b4d5cfafb90e7139c18484538950284b8d1da56ccb0fc7f65c8030c7c67a60f377145d1d2bed254bf950374bdb09691ee63e682f75',
+  ),
+);
